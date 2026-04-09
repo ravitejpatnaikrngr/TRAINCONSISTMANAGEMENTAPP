83c263f21f38d4b6053b8ab45e3dcdd4de61ad9c
ravitejpatnaikrngr <rp7108@srmist.edu.in>
2026-04-09 10:08:27 +0530

    UC1:Linear Search for Bogie ID

--- src/TRAINCONSISTMANAGEMENTAPP.java
+++ src/TRAINCONSISTMANAGEMENTAPP.java
@@ -3,10 +3,18 @@ import java.util.*;
 public class TRAINCONSISTMANAGEMENTAPP {
     public static void main(String[] args) {
 
-        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
+        String[] bogieIds = {"BG101","BG205","BG309","BG412","BG550"};
+        String searchKey = "BG309";
 
-        Arrays.sort(bogieNames);
+        boolean found = false;
 
-        System.out.println(Arrays.toString(bogieNames));
+        for (String id : bogieIds) {
+            if (id.equals(searchKey)) {
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println(found);
     }
 }
